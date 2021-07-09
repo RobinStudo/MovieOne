bcc966071fc54ad92cc87076e68bdd52da820c38
WF3 <wf3@wf3.fr>
2021-07-09 16:54:19 +0200

    Exemple : service

--- src/app/pages/list/list.component.ts
+++ src/app/pages/list/list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MovieService } from '../../services/movie.service';
 
 @Component({
   selector: 'app-list',
@@ -8,41 +9,8 @@ import { Component, OnInit } from '@angular/core';
 export class ListComponent implements OnInit {
   movies: Array<any> = [];
 
-  constructor(){
-    this.movies = [
-      {
-        id: 1,
-        title: "The Rock",
-        director: "Michel Bay",
-        picture: "https://images-na.ssl-images-amazon.com/images/I/51Qorl8946L._AC_.jpg",
-        rate: 4.2,
-        releasedAt: "1993-05-22",
-      },
-      {
-        id: 2,
-        title: "Pulp Fiction",
-        director: "Quentin Tarantino",
-        picture: "https://static.posters.cz/image/750/affiches-et-posters/pulp-fiction-cover-i1288.jpg",
-        rate: 3.5,
-        releasedAt: "1995-06-22",
-      },
-      {
-        id: 3,
-        title: "Idependence Day",
-        director: "Rolland Emrich",
-        picture: "https://images-na.ssl-images-amazon.com/images/I/51K8V3J8EML._AC_SY445_.jpg",
-        rate: 1.8,
-        releasedAt: "2000-01-22",
-      },
-      {
-        id: 4,
-        title: "Titanic",
-        director: "James Cameron",
-        picture: "https://i.ebayimg.com/images/g/OEkAAOSwIUddNNDU/s-l300.jpg",
-        rate: 2.5,
-        releasedAt: "1998-01-22",
-      },
-    ];
+  constructor(private movieService: MovieService){
+    this.movies = this.movieService.getAll();
   }
 
   ngOnInit(): void {
